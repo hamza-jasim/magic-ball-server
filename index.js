b14ef87545c64f8f361b57ab425b6312e91b8f31
hamza-jasim <hamza201317@gmail.com>
2026-03-25 07:01:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post("/api/answer", async (req, res) => {
 });
 
 // 🔴 مهم جداً ل Railway
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
